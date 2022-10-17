4b889e41ded5169f9f8104e8d5b3506070f23a73
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2022-10-17 23:39:09 +0800

    initial commit

---
+++ numbers.py
@@ -0,0 +1,25 @@
+print(1 + (2 * 3) - 4 % 5)
+
+from math import *
+my_num = 5
+
+print(my_num)
+
+# convert a number to a string
+print(str(my_num) + " is a number")
+
+# absolute value
+print(abs(my_num))
+
+# power (5 raised to power of 2)
+print(pow(5, 2))
+
+# return larger of 2 numbers
+print(max(1, 3))
+print(min(1, 3))
+
+print(round(6.14))
+print(floor(6.59))
+
+# math functions in python
+print(sqrt(49))
---
+++ strings.py
@@ -0,0 +1,16 @@
+# the '\' character acts as an escape character
+print("hello\nworld")
+
+phrase = "lorem ipsum lorem"
+print(phrase.upper())
+
+# get the length of a string
+print(len(phrase))
+
+# get the first character, use the index
+print(phrase[0])
+
+print(phrase.index("o"))
+
+# replace something in a string
+print(phrase.replace("lorem", "siomai"))
